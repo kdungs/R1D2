35030fce4aa895e8b80dcb78dd874ffbc95b5744
Kevin Dungs <kevin@dun.gs>
2016-01-23 00:39:13 +0100

    Ordered enum no longer needed.

--- r1/types.py
+++ r1/types.py
@@ -3,28 +3,6 @@ from collections import namedtuple
 from functools import partial
 
 
-class OrderedEnum(Enum):
-    def __ge__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value >= other.value
-        return NotImplemented
-
-    def __gt__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value > other.value
-        return NotImplemented
-
-    def __le__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value <= other.value
-        return NotImplemented
-
-    def __lt__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value < other.value
-        return NotImplemented
-
-
 def valid_string_for_enum(enum, s):
     return s in enum.__members__
 
@@ -33,12 +11,12 @@ def enum_from_string(enum, s):
     return enum.__members__[s]
 
 
-Restaurant = OrderedEnum('Restaurant', ['r1', 'r2'])
+Restaurant = Enum('Restaurant', ['r1', 'r2'])
 valid_string_for_restaurant = partial(valid_string_for_enum, Restaurant)
 restaurant_from_string = partial(enum_from_string, Restaurant)
 
 
-DishType = OrderedEnum('DishType', ['menu1', 'menu2', 'vegetarian',
+DishType = Enum('DishType', ['menu1', 'menu2', 'vegetarian',
                                     'speciality', 'grill', 'pasta', 'pizza'])
 valid_string_for_dishtype = partial(valid_string_for_enum, DishType)
 dishtype_from_string = partial(enum_from_string, DishType)
