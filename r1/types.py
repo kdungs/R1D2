8d3f5c411531ff08d891000e62e647c368675f32
Kevin Dungs <kevin@dun.gs>
2016-01-22 15:22:19 +0100

    Implement types in separate file.
    Also add unit tests and Travis setup.

---
+++ r1/types.py
@@ -0,0 +1,26 @@
+from enum import Enum
+from collections import namedtuple
+from functools import partial
+
+
+def valid_string_for_enum(enum, s):
+    return s in enum.__members__
+
+
+def enum_from_string(enum, s):
+    return enum.__members__[s]
+
+
+Restaurant = Enum('Restaurant', ['r1', 'r2'])
+valid_string_for_restaurant = partial(valid_string_for_enum, Restaurant)
+restaurant_from_string = partial(enum_from_string, Restaurant)
+
+
+DishType = Enum('DishType', ['menu1', 'menu2', 'vegetarian', 'speciality',
+                             'grill', 'pasta', 'pizza'])
+valid_string_for_dishtype = partial(valid_string_for_enum, DishType)
+dishtype_from_string = partial(enum_from_string, DishType)
+
+
+MenuItem = namedtuple('MenuItem', ['restaurant', 'date', 'type',
+                                   'name', 'price'])
---
+++ tests/test_types.py
@@ -0,0 +1,60 @@
+import unittest
+
+from r1 import types
+from datetime import date
+
+
+class TestRestaurant(unittest.TestCase):
+    def test_valid_string(self):
+        self.assertTrue(types.valid_string_for_restaurant('r1'))
+        self.assertTrue(types.valid_string_for_restaurant('r2'))
+        self.assertFalse(types.valid_string_for_restaurant(None))
+        self.assertFalse(types.valid_string_for_restaurant(''))
+        self.assertFalse(types.valid_string_for_restaurant('r3'))
+        self.assertFalse(types.valid_string_for_restaurant('r1r1r1r1'))
+
+    def test_from_string(self):
+        self.assertEqual(types.Restaurant.r1,
+                         types.restaurant_from_string('r1'))
+        self.assertEqual(types.Restaurant.r2,
+                         types.restaurant_from_string('r2'))
+        self.assertRaises(KeyError, types.restaurant_from_string, None)
+        self.assertRaises(KeyError, types.restaurant_from_string, '')
+        self.assertRaises(KeyError, types.restaurant_from_string, 'r3')
+
+
+class TestDishType(unittest.TestCase):
+    def test_valid_string(self):
+        valid_strs = ['menu1', 'menu2', 'vegetarian', 'speciality', 'grill',
+                      'pasta', 'pizza']
+        for valid_str in valid_strs:
+            self.assertTrue(types.valid_string_for_dishtype(valid_str))
+        self.assertFalse(types.valid_string_for_dishtype(None))
+        self.assertFalse(types.valid_string_for_dishtype(''))
+        self.assertFalse(types.valid_string_for_dishtype('cake'))
+        self.assertFalse(types.valid_string_for_dishtype('menu1000'))
+
+    def test_from_string(self):
+        expected = {'menu1': types.DishType.menu1,
+                    'menu2': types.DishType.menu2,
+                    'vegetarian': types.DishType.vegetarian,
+                    'speciality': types.DishType.speciality,
+                    'grill': types.DishType.grill,
+                    'pasta': types.DishType.pasta,
+                    'pizza': types.DishType.pizza}
+        for k, v in expected.items():
+            self.assertEqual(v, types.dishtype_from_string(k))
+        self.assertRaises(KeyError, types.dishtype_from_string, None)
+        self.assertRaises(KeyError, types.dishtype_from_string, '')
+        self.assertRaises(KeyError, types.dishtype_from_string, 'cake')
+
+
+class TestMenuItem(unittest.TestCase):
+    def test_creation(self):
+        mi = types.MenuItem(types.Restaurant.r1, date.today(),
+                            types.DishType.menu1, 'Shit with sauce', 13.37)
+        self.assertEqual(types.Restaurant.r1, mi.restaurant)
+        self.assertEqual(date.today(), mi.date)
+        self.assertEqual(types.DishType.menu1, mi.type)
+        self.assertEqual('Shit with sauce', mi.name)
+        self.assertEqual(13.37, mi.price)
